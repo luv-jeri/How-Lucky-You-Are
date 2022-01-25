6770a24412ec70a3b2ef2c2421bac9acbab148c4
luv jeri <hiluvjeri@gmail.com>
2022-01-25 18:06:17 +0530

    fixes

--- script.js
+++ script.js
@@ -1,45 +1,42 @@
-let luckyNumber = Math.floor(
-  Math.random() * 20
-);
+let luckyNumber = Math.floor(Math.random() * 20);
 console.log(luckyNumber);
+
 let userInput = 0;
 let score = 20;
 let highScore = 0;
 
+const displayMessage = (message) => {
+  document.querySelector('.message').innerHTML = message;
+};
+
+// DRY -> Don't Repeat Yourself
+
 document
   .querySelector('#check')
-  .addEventListener(
-    'click',
-    function () {
-      userInput =
-        document.querySelector(
-          '.guess'
-        ).value;
-      if (userInput == luckyNumber) {
-        document.querySelector(
-          '.message'
-        ).innerHTML = 'You Win!';
-        document.querySelector(
-          'body'
-        ).style.backgroundColor =
-          '#146356';
-        highScore = score;
-      } else {
-        if (userInput > luckyNumber) {
-          document.querySelector(
-            '.message'
-          ).innerHTML = 'Too High';
-        }
-        if (userInput < luckyNumber) {
-          document.querySelector(
-            '.message'
-          ).innerHTML = 'Too Low';
-        }
+  .addEventListener('click', function () {
+    userInput = document.querySelector('.guess').value;
 
-        score--;
+    userInput = Number(userInput); // "3" -> 3
+
+    if (userInput === luckyNumber) {
+      displayMessage('You Win!');
+      document.querySelector('.message').style = 'color : #D67D3E';
+      document.querySelector('body').style.backgroundColor =
+        '#146356';
+
+      highScore = score;
+    } else {
+      if (userInput > luckyNumber) {
+        displayMessage('Too High');
       }
-      document.querySelector(
-        '.score'
-      ).innerHTML = score;
+
+      if (userInput < luckyNumber) {
+        displayMessage('Too Low');
+      }
+
+      document.querySelector('.message').style = 'color: red;';
+      score--;
     }
-  );
+
+    document.querySelector('.score').innerHTML = score;
+  });
