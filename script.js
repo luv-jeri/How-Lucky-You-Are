13aa4f54638a7551a9a35fdc2d2ef367f9bdfa15
luv jeri <hiluvjeri@gmail.com>
2022-01-28 19:34:10 +0530

    -

--- script.js
+++ script.js
@@ -1,45 +1,64 @@
-let luckyNumber = Math.floor(
-  Math.random() * 20
-);
-console.log(luckyNumber);
-let userInput = 0;
+let luckyNumber = Math.floor(Math.random() * 20); // Generates a random number between 0 and 19
+
 let score = 20;
 let highScore = 0;
 
-document
-  .querySelector('#check')
-  .addEventListener(
-    'click',
-    function () {
-      userInput =
-        document.querySelector(
-          '.guess'
-        ).value;
-      if (userInput == luckyNumber) {
-        document.querySelector(
-          '.message'
-        ).innerHTML = 'You Win!';
-        document.querySelector(
-          'body'
-        ).style.backgroundColor =
-          '#146356';
-        highScore = score;
-      } else {
-        if (userInput > luckyNumber) {
-          document.querySelector(
-            '.message'
-          ).innerHTML = 'Too High';
-        }
-        if (userInput < luckyNumber) {
-          document.querySelector(
-            '.message'
-          ).innerHTML = 'Too Low';
-        }
+let scoreEl = document.querySelector('.score');
+const message = document.querySelector('.message');
+let highScoreEl = document.querySelector('.highscore');
+let guessEl = document.querySelector('.guess');
+let body = document.querySelector('body');
 
-        score--;
-      }
-      document.querySelector(
-        '.score'
-      ).innerHTML = score;
+scoreEl.innerHTML = score;
+highScoreEl.innerHTML = highScore;
+
+console.log(luckyNumber);
+
+const onClick = function () {
+  const userInput = guessEl.value;
+
+  if (luckyNumber == userInput) {
+    message.innerHTML = 'You guessed the correct number!';
+
+    body.style = 'background-color: #219F94';
+    if (score > highScore) {
+      highScore = score;
+      highScoreEl.innerHTML = highScore;
     }
-  );
+  } else {
+    score--;
+
+    scoreEl.innerHTML = score;
+
+    if (score == 0) {
+      document.querySelector('#idtitle').innerHTML =
+        'You have lost try again!';
+      body.style = 'background-color: #E84855';
+    }
+
+    if (userInput < luckyNumber) {
+      message.innerHTML = 'Too low!';
+      message.style = 'color: #B8405E';
+    }
+    if (userInput > luckyNumber) {
+      message.style = 'color: #B8405E';
+      message.innerHTML = 'Too high!';
+    }
+  }
+};
+
+document.querySelector('#check').addEventListener('click', onClick);
+
+document
+  .querySelector('#again')
+  .addEventListener('click', function () {
+    document.querySelector('#idtitle').innerHTML =
+      'How lucky are you?';
+    luckyNumber = Math.floor(Math.random() * 20);
+    console.log(luckyNumber);
+    score = 20;
+    scoreEl.innerHTML = score;
+    message.innerHTML = 'Start guessing!';
+    guessEl.value = 0;
+    document.querySelector('body').style = 'background-color: #222';
+  });
