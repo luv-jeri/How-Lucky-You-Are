a86f6cdfdd970bf07ccd3723dd310e2e73441fc5
luv-jeri <luvvjeri@gmail.com>
2022-03-02 21:40:51 +0530

    -

--- script.js
+++ script.js
@@ -1,64 +1,58 @@
-let luckyNumber = Math.floor(Math.random() * 20); // Generates a random number between 0 and 19
+//! Here is a code to generate a random number between 1 and 20.
+var luckyNumber = Math.random() * 20; //` * generates a random number between 0 and 1 and multiply it by 20.
+luckyNumber = Math.floor(luckyNumber); //` Removing the decimal part.
 
-let score = 20;
-let highScore = 0;
+// ! Here is a variable to store the score.
+var scoreNumber = 20;
+var highScore = 0;
 
-let scoreEl = document.querySelector('.score');
-const message = document.querySelector('.message');
-let highScoreEl = document.querySelector('.highscore');
-let guessEl = document.querySelector('.guess');
-let body = document.querySelector('body');
+//! Here we are garbing the input field and the button by ID
+var guessing =
+  document.getElementById('guessing');
+var check = document.getElementById('check');
+var message = document.getElementById('message');
+var score = document.getElementById('score');
+var main = document.getElementById('main');
+var highscore =
+  document.getElementById('highscore');
+var again = document.getElementById('again');
 
-scoreEl.innerHTML = score;
-highScoreEl.innerHTML = highScore;
+//! Here is a function to check if the number is correct
+function checkNumber() {
+  var userNumber = guessing.value; //` Here we are grabbing the value of the input field.
+
+  if (luckyNumber < userNumber) {
+    message.innerHTML = 'Your number is too high';
+    scoreNumber = scoreNumber - 1;
+    score.innerHTML = scoreNumber;
+    main.style.backgroundColor = '#D82148';
+  }
+  if (luckyNumber > userNumber) {
+    message.innerHTML = 'Your number is too low';
+    scoreNumber = scoreNumber - 1;
+    score.innerHTML = scoreNumber;
+    main.style.backgroundColor = '#D82148';
+  }
+  if (luckyNumber == userNumber) {
+    message.innerHTML = 'You got it!';
+    main.style.backgroundColor = '#019267';
+    highscore.innerHTML = scoreNumber;
+    scoreNumber = 20;
+    score.innerHTML = scoreNumber;
+  }
+}
+
+function startAgain() {
+  luckyNumber = Math.random() * 20; //` * generates a random number between 0 and 1 and multiply it by 20.
+  luckyNumber = Math.floor(luckyNumber);
+  scoreNumber = 20;
+  score.innerHTML = scoreNumber;
+  console.log(luckyNumber);
+  main.style.backgroundColor = '#222';
+}
+
+//! Here are running the function when the button is clicked -> checkNumber
+check.onclick = checkNumber;
+again.onclick = startAgain;
 
 console.log(luckyNumber);
-
-const onClick = function () {
-  const userInput = guessEl.value;
-
-  if (luckyNumber == userInput) {
-    message.innerHTML = 'You guessed the correct number!';
-
-    body.style = 'background-color: #219F94';
-    if (score > highScore) {
-      highScore = score;
-      highScoreEl.innerHTML = highScore;
-    }
-  } else {
-    score--;
-
-    scoreEl.innerHTML = score;
-
-    if (score == 0) {
-      document.querySelector('#idtitle').innerHTML =
-        'You have lost try again!';
-      body.style = 'background-color: #E84855';
-    }
-
-    if (userInput < luckyNumber) {
-      message.innerHTML = 'Too low!';
-      message.style = 'color: #B8405E';
-    }
-    if (userInput > luckyNumber) {
-      message.style = 'color: #B8405E';
-      message.innerHTML = 'Too high!';
-    }
-  }
-};
-
-document.querySelector('#check').addEventListener('click', onClick);
-
-document
-  .querySelector('#again')
-  .addEventListener('click', function () {
-    document.querySelector('#idtitle').innerHTML =
-      'How lucky are you?';
-    luckyNumber = Math.floor(Math.random() * 20);
-    console.log(luckyNumber);
-    score = 20;
-    scoreEl.innerHTML = score;
-    message.innerHTML = 'Start guessing!';
-    guessEl.value = 0;
-    document.querySelector('body').style = 'background-color: #222';
-  });
