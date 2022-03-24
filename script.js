d0052486091a58a8faf2e3253e5cce7513296258
luv-jeri <luvvjeri@gmail.com>
2022-03-24 21:41:36 +0530

    -

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
-// let j = 0;
-// let i = 0;
+// const els = document.getElementsByClassName('c');
 
-// for (j = j + i; i < 3; j = j + i) {
-//   console.log('Loop', i);
-//   i++;
+// for (let i = 0; i < els.length; i++) {
+//   els[i].style.backgroundColor = 'red';
 // }
 
-// console.log('I', i);
-// console.log('J', j);
-
-// do {
-//   console.log('Loop');
-// } while (false);
-
-// const table = (num) => {
-//   let i = 0;
-
-//   while (i <= 10) {
-//     console.log(i * num);
-//     i++;
-//   }
+// const forEachWala = (element, index, array) => {
+//   console.log(element, index, array);
 // };
 
-// table(20356236);
-// table(46);
-// table(345);
+// arr.forEach(forEachWala);
 
-// const arr = ['a', 'b'];
+// const clone = arr.map((element, index, array) => {
+//   return element;
+// });
 
-// for (let i = 0; i < arr.length; i++) {
-//   console.log(arr[i]);
-// }
+// const arr = [1, 2, 3, 4, 5];
 
-// for (let i = 0; i < 100; i = i + 5) {
-//   if (i % 2 === 0) {
-//     console.log(i);
-//   }
-// }
+// const fun = (accumulator, currentValue, currentIndex, array) => {
+//   accumulator = accumulator + currentValue;
+//   return accumulator;
+// };
+
+// const total = arr.reduce(fun, 0);
+
+// //  is john is doe is jane is doe
+
+// const fun = (accumulator, currentValue, currentIndex, array) => {
+//   accumulator.push(currentValue);
+//   return accumulator;
+// };
+
+// const finalStr = arr.reduce(fun, []);
+
+// console.log(finalStr);
+
+const arr = [10, 5, 65, 23, 7, 345, 3454778, 23, 5, 6, 7, 8, 9, 10];
+
+const str = arr.sort();
+
+console.log(str);
