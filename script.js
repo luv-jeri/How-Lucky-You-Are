16ee2f17723d706acbd11b55b45db6234f57ec95
luv-jeri <luvvjeri@gmail.com>
2022-04-05 21:52:16 +0530

    -

--- script.js
+++ script.js
@@ -1,35 +1,96 @@
-const rollADice = () => {
-  return Math.floor(Math.random() * 6) + 1;
+const cardGenerator = (element) => {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  card.innerHTML = `
+        <img src="${element.avatar}" alt="${element.first_name}">
+        <h2>${element.first_name} ${element.last_name}</h2>
+        <p>${element.email}</p>
+        `;
+  return card;
 };
 
-const assignDice = (name) => {
-  document.querySelector('#player').innerHTML = name;
-};
+const body = document.querySelector('body');
 
-let player1 = 0;
-let player2 = 0;
+// fetch('https://reqres.in/api/users?page=2')
+//   .then((res) => {
+//     console.log(res);
 
-let turn = true;
+//     const data = res.json();
 
-const findWinner = () => {
-  if (player1 > 50 || player2 > 50) {
-    assignDice(`Player ${turn ? '1' : '2'} wins!`);
+//     console.log(data);
+
+//     data.then((data_) => {
+//       console.log(data_);
+//       console.log(data_.data);
+
+//       data_.data.forEach((element) => {
+//         const card = cardGenerator(element);
+//         body.append(card);
+//       });
+//     });
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+const get = async () => {
+  const res = await fetch('https://reqres.in/api/users?page=1');
+  const data = await res.json();
+
+  console.log(' Inside the get function ', data.data);
+
+  if (data.data.length === 0) {
+    console.log('No data');
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+        <h2>No data</h2>
+        `;
+    body.append(card);
+    return;
   }
+
+  data.data.forEach((element) => {
+    const card = cardGenerator(element);
+    body.append(card);
+  });
 };
 
-document.querySelector('#btn').addEventListener('click', () => {
-  console.log('turn', turn);
-  const dice = rollADice();
-  if (turn) {
-    player1 = player1 + dice;
-    assignDice(`turn Player 1: ${dice}`);
-  } else {
-    player2 = player2 + dice;
-    assignDice(`turn Player 2: ${dice}`);
-  }
-  turn = !turn;
-  findWinner();
+get();
 
-  document.querySelector('#p1').innerHTML = `Player 1: ${player1}`;
-  document.querySelector('#p2').innerHTML = `Player 2: ${player2}`;
-});
+// const pr = new Promise((resolve, reject) => {
+//   reject('Error 😡😡');
+//   resolve('Hello');
+// });
+
+// pr.then((data) => {
+//   console.log(data);
+// });
+
+// pr.catch((error) => {
+//   console.log(error);
+// });
+
+// const get = async () => {
+//   try {
+//     var data = await pr;
+//     console.log('resolved data', data);
+//   } catch (error) {
+//     console.log('rejected data', error);
+//   }
+// };
+
+// get();
+
+// try {
+//   const age = prompt('How old are you?');
+//   if (age > 18) {
+//     alert('You are old enough');
+//   } else {
+//     throw 'You are not old enough'
+//   }
+// } catch (err) {
+//   console.log('Something went wrong 🥲🥲');
+//   console.log(err);
+// }
